ea72ac3f027ebad09d77a505b22eb34555856a58
Nathalie Wong <nathalie.wong@wildfireapp.com>
2012-07-01 11:41:58 +0100

    DeptController complete, index and new views complete. Stuck

--- app/controllers/depts_controller.rb
+++ app/controllers/depts_controller.rb
@@ -1,5 +1,5 @@
 class DeptsController < ApplicationController
-  
+
   def index
     @depts = Dept.all
 
@@ -14,6 +14,59 @@ class DeptsController < ApplicationController
 
     respond_to do |format|
       format.html
+      format.json { render :json => @dept }
+    end
+  end
+
+  def new
+    @dept = Dept.new
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @dept }
+    end
+  end
+
+  def create
+    @dept = Dept.new(params[:id])
+
+    respond_to do |format|
+      if @dept.save(params[:id])
+         format.html { redirect_to :action => 'index' }
+         format.json { render :json => @dept }
+
+      else
+         format.html { render :action => 'new' }
+         format.json { render :json => @dept }
+      end
+    end
+  end
+
+  def edit
+    @dept = Dept.find(params[:id])
+  end
+
+  def update
+    @dept = Dept.find(params[:id])
+
+    respond_to do |format|
+      if @dept.update_attributes(params[:id])
+         format.html { redirect_to :action => 'show', :id => @dept }
+         format.json { render :json => @dept }
+
+      else
+         format.html { redirect_to :action => 'edit' }
+         format.json { render :json => @dept }
+      end
+    end
+  end
+
+  def destroy
+    @dept = Dept.find(params[:id])
+    @dept.destroy
+
+    respond_to do |format|
+      format.html { redirect_to :action => 'index' }
       format.json { render :json => @dept }
     end
   end
